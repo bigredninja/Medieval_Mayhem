3b4fa4cd8c48ebf9d6d30c9675c9c80f66c86c09
chase <chase@HDCPC2>
2021-04-08 18:29:46 -0700

    sdfghjk

--- src/medieval_Mayhem/GamePanel.java
+++ src/medieval_Mayhem/GamePanel.java
@@ -1,5 +1,12 @@
 package medieval_Mayhem;
 
-public class GamePanel {
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	@Override
+	public void paintComponent(Graphics g){
+		g.fillRect(0, 500, 60,100 );
+	}
 }
--- src/medieval_Mayhem/Medieval_Mayhem.java
+++ src/medieval_Mayhem/Medieval_Mayhem.java
@@ -6,13 +6,17 @@ public class Medieval_Mayhem {
 	public static int WIDTH = 500;
 	public static int HEIGHT = 800;
 	JFrame frame;	
+	GamePanel panel;
 	Medieval_Mayhem(){
 		frame = new JFrame();
+		panel = new GamePanel();
 	}
 	void setup(){
+		frame.add(panel);
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	public static void main(String[] args) {
 		Medieval_Mayhem mayhem = new Medieval_Mayhem();
