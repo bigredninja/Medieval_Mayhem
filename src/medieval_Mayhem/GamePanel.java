d33b90980b4c1a9751cad893f33278420cdf2275
chase <chase@HDCPC2>
2021-06-02 19:59:38 -0700

    defregthfgthgthjk

--- src/medieval_Mayhem/GamePanel.java
+++ src/medieval_Mayhem/GamePanel.java
@@ -21,160 +21,190 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener, Mo
 	public static boolean gotImage = false;
 	final int MENU = 0;
 	final int MAP = 1;
-    final int GAME = 2;
-    final int END = 3;
-    int currentState = MENU;
-    Font titleFont;
+	final int GAME = 2;
+	final int END = 3;
+	final int STAGE1X = 200;
+	final int STAGE1Y = 440;
+	final int STAGESIZEX = 150; 
+	final int STAGESIZEY = 50;
+	final int STAGE2X = 519;
+	final int STAGE2Y = 410;
+	final int STAGE3X = 519;
+	final int STAGE3Y = 260;
+	int currentState = MENU;
+	Font titleFont;
 	Font tiitleFont;
 	Font tiiitleFont;
 	Font EndFont;
 	Timer frameDraw;
-    GamePanel(){
-		titleFont = new Font("Arial", Font.PLAIN, 48);
+	GamePanel(){
+		titleFont = new Font("Arial", Font.PLAIN, 44);
 		tiitleFont = new Font("Arial", Font.PLAIN, 15);
 		tiiitleFont = new Font("Arial", Font.PLAIN, 15);
 		EndFont = new Font("Arial", Font.PLAIN, 48);
 		frameDraw = new Timer(1000/60,this);
 		frameDraw.start();
-			loadImage ("map.jpg");
-		
+		loadImage ("map.jpg");
+
 	}
 	@Override
 	public void paintComponent(Graphics g){
 		if(currentState == MENU){
-		    drawMenuState(g);
+			drawMenuState(g);
 		}else if(currentState == MAP){
-		    drawMapState(g);
+			drawMapState(g);
 		}else if(currentState == GAME){
-		    drawGameState(g);
+			drawGameState(g);
 		}else if(currentState == END){
-		    drawEndState(g);
+			drawEndState(g);
 		}
 	}
-	 void updateMenuState() {  }
-	 void updateMapState()  {  }
-	 void updateGameState() {  }
-	 void updateEndState()  {  }
-	 void drawMenuState(Graphics g) { 
-		 g.setColor(Color.BLUE);
-		 g.fillRect(0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT);
-	 }
-	 void drawMapState(Graphics g)  { 
-		 if (gotImage) {
-				g.drawImage(image, 0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT, null);
-			} else {
-				g.setColor(Color.BLACK);
-				g.fillRect(0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT);
+	void updateMenuState() {  }
+	void updateMapState()  {  }
+	void updateGameState() {  }
+	void updateEndState()  {  }
+	void drawMenuState(Graphics g) { 
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT);
+	}
+	void drawMapState(Graphics g)  { 
+		if (gotImage) {
+			g.drawImage(image, 0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT, null);
+		} else {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, Medieval_Mayhem.WIDTH, Medieval_Mayhem.HEIGHT);
+		}
+		g.setColor(Color.RED);
+		g.drawRect(STAGE1X, STAGE1Y, STAGESIZEX, STAGESIZEY);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Stage 1", STAGE1X, STAGE1Y + STAGESIZEY / 4 * 3);
+
+		g.setColor(Color.RED);
+		g.drawRect(STAGE2X, STAGE2Y, STAGESIZEX, STAGESIZEY);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Stage 2", STAGE2X, STAGE2Y + STAGESIZEY / 4 * 3);
+
+		g.setColor(Color.RED);
+		g.drawRect(STAGE3X, STAGE3Y, STAGESIZEX, STAGESIZEY);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Stage 3", STAGE3X, STAGE3Y + STAGESIZEY / 4 * 3);
+	}
+	void drawGameState(Graphics g) {  }
+	void drawEndState(Graphics g)  {  }
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+			System.out.println("enter");
+			if (currentState == END) {
+				currentState = MENU;
+			} 
+			else if (currentState == MENU) {
+				currentState = MAP;
 			}
-		 g.setColor(Color.RED);
-			g.fillRect(400, 375, 50, 50);
-	 }
-	 void drawGameState(Graphics g) {  }
-	 void drawEndState(Graphics g)  {  }
-	 
-	 @Override
-	 public void keyPressed(KeyEvent e) {
-		 // TODO Auto-generated method stub
+		}   
+		if (e.getKeyCode()==KeyEvent.VK_UP) {
+			System.out.println("UP");
+			//rocketship.UP = true;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			System.out.println("LEFT");
+			//rocketship.LEFT = true;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			System.out.println("RIGHT");
+			//rocketship.RIGHT = true;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_DOWN) {
+			System.out.println("DOWN");
+			//rocketship.DOWN = true;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+			//objectManager.addProjectile(rocketship.getProjectile());
+		}
+	}
 
-		 if (e.getKeyCode()==KeyEvent.VK_ENTER) {
-			 System.out.println("enter");
-			 if (currentState == END) {
-				 currentState = MENU;
-			 } 
-			 else if (currentState == MENU) {
-				 currentState = MAP;
-			 }
-		 }   
-		 if (e.getKeyCode()==KeyEvent.VK_UP) {
-			 System.out.println("UP");
-			 //rocketship.UP = true;
-		 }
-		 if (e.getKeyCode()==KeyEvent.VK_LEFT) {
-			 System.out.println("LEFT");
-			 //rocketship.LEFT = true;
-		 }
-		 if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
-			 System.out.println("RIGHT");
-			 //rocketship.RIGHT = true;
-		 }
-		 if (e.getKeyCode()==KeyEvent.VK_DOWN) {
-			 System.out.println("DOWN");
-			 //rocketship.DOWN = true;
-		 }
-		 if (e.getKeyCode()==KeyEvent.VK_SPACE) {
-			 //objectManager.addProjectile(rocketship.getProjectile());
-		 }
-	 }
-
-	 void loadImage(String imageFile) {
-		 if (needImage) {
-			 try {
-				 image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
-				 gotImage = true;
-				 System.out.println("Its working");
-			 } catch (Exception e) {
-				 e.printStackTrace();
-			 }
-			 needImage = false;
-		 }
-	 }
-	 @Override
-	 public void actionPerformed(ActionEvent e) {
-		 // TODO Auto-generated method stub
-		 if(currentState == MENU){
-				updateMenuState();
-			}else if(currentState == MAP){
-				updateMapState();
-			}else if(currentState == GAME){
-				updateGameState();
-			}else if(currentState == END){
-				updateEndState();
+	void loadImage(String imageFile) {
+		if (needImage) {
+			try {
+				image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+				gotImage = true;
+				System.out.println("Its working");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			repaint();
-	 }
-	 @Override
-	 public void keyTyped(KeyEvent e) {
-		 // TODO Auto-generated method stub
+			needImage = false;
+		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+			updateMenuState();
+		}else if(currentState == MAP){
+			updateMapState();
+		}else if(currentState == GAME){
+			updateGameState();
+		}else if(currentState == END){
+			updateEndState();
+		}
+		repaint();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 
-	 }
-	 @Override
-	 public void keyReleased(KeyEvent e) {
-		 // TODO Auto-generated method stub
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
-	 }
+	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		if (mapClicked(e,400,450,375,425)) {
+		if (mapClicked(e,STAGE1X,STAGE1X + STAGESIZEX,STAGE1Y,STAGE1Y + STAGESIZEY)) {
 			System.out.println("clicked");
 		}
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	boolean mapClicked(MouseEvent e,int xmin,int xmax,int ymin,int ymax) {
-		System.out.println(e.getX() + " " + e.getY());
-		if (e.getX() >= xmin + 7 && e.getX() <= xmax + 7 && e.getY() >= ymin + 30 && e.getY() <= ymax + 30) {
-			return true; 
+		else if (mapClicked(e,STAGE2X,STAGE2X + STAGESIZEX,STAGE2Y,STAGE2Y + STAGESIZEY)) {
+			System.out.println("clicked");
+
 		}
-		return false;
-		
+		else if (mapClicked(e,STAGE3X,STAGE3X + STAGESIZEX,STAGE3Y,STAGE3Y + STAGESIZEY)) {
+			System.out.println("clicked"); }
+			
+		} 
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+		boolean mapClicked(MouseEvent e,int xmin,int xmax,int ymin,int ymax) {
+			System.out.println(e.getX() + " " + e.getY());
+			if (e.getX() >= xmin + 7 && e.getX() <= xmax + 7 && e.getY() >= ymin + 30 && e.getY() <= ymax + 30) {
+				return true; 
+			}
+			return false;
+
+		}
 	}
-}
